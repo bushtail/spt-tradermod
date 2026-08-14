d8adbdbc47e735650881dc3e56f69903bd8f3084
bushtail <evannosich@icloud.com>
2026-08-14 16:22:31 -0400

    obj gone

---
+++ TraderMod.Shared/SubtitlesUI.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+
+namespace TraderMod.Shared;
+
+public class SubtitlesUI : MonoBehaviour
+{
+    private TMP_Text _text;
+    private CanvasGroup _canvasGroup;
+
+    public void SetSubtitle(string subtitle)
+    {
+        
+    }
+}
